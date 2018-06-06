49f164b061d861386e59fccbbdbbec88cff11be9
Martin Peres <martin.peres@linux.intel.com>
2018-06-06 17:26:34 +0300

    modesetting: make drmmode_shadow_allocate more generic
    To do so, introduce a drmmode_scanout_rec structure that contains:
     - the DRM BO
     - the DRM FB id
    
    We then take the generic part of drmmode_shadow_allocate that creates a
    scanable buffer and move it to drmmode_scanout_allocate().
    
    Signed-off-by: Martin Peres <martin.peres@linux.intel.com>

--- hw/xfree86/drivers/modesetting/present.c
+++ hw/xfree86/drivers/modesetting/present.c
@@ -238,7 +238,7 @@ ms_present_check_flip(RRCrtcPtr crtc,
         drmmode_crtc_private_ptr drmmode_crtc = config->crtc[i]->driver_private;
 
         /* Don't do pageflipping if CRTCs are rotated. */
-        if (drmmode_crtc->rotate_bo.gbm)
+        if (drmmode_crtc->shadow_rotated.bo.gbm)
             return FALSE;
 
         if (ms_crtc_on(config->crtc[i]))
