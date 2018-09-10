f27754d2b3f119bd73a1c47692be8c885a1f7695
Adam Jackson <ajax@redhat.com>
2018-09-10 15:53:16 -0400

    xfree86: Remove xf86AllocateScrnInfoPrivateIndex
    Nothing's using this anymore, hooray.
    
    Signed-off-by: Adam Jackson <ajax@redhat.com>

--- hw/xfree86/common/xf86Helper.c
+++ hw/xfree86/common/xf86Helper.c
@@ -61,8 +61,6 @@
 #include <sys/resource.h>
 #endif
 
-static int xf86ScrnInfoPrivateCount = 0;
-
 /* Add a pointer to a new DriverRec to xf86DriverList */
 
 void
@@ -185,7 +183,6 @@ xf86AllocateScreen(DriverPtr drv, int flags)
     }
 
     pScrn->origIndex = pScrn->scrnIndex;      /* This never changes */
-    pScrn->privates = xnfcalloc(sizeof(DevUnion), xf86ScrnInfoPrivateCount);
     /*
      * EnableDisableFBAccess now gets initialized in InitOutput()
      * pScrn->EnableDisableFBAccess = xf86EnableDisableFBAccess;
@@ -245,8 +242,6 @@ xf86DeleteScreen(ScrnInfoPtr pScrn)
     if (pScrn->drv)
         pScrn->drv->refCount--;
 
-    free(pScrn->privates);
-
     xf86ClearEntityListForScreen(pScrn);
 
     free(pScrn);
@@ -271,37 +266,6 @@ xf86DeleteScreen(ScrnInfoPtr pScrn)
             /* Also need to take care of the screen layout settings */
         }
     }
-}
-
-/*
- * Allocate a private in ScrnInfoRec.
- */
-
-int
-xf86AllocateScrnInfoPrivateIndex(void)
-{
-    int idx, i;
-    ScrnInfoPtr pScr;
-    DevUnion *nprivs;
-
-    idx = xf86ScrnInfoPrivateCount++;
-    for (i = 0; i < xf86NumScreens; i++) {
-        pScr = xf86Screens[i];
-        nprivs = xnfreallocarray(pScr->privates,
-                                 xf86ScrnInfoPrivateCount, sizeof(DevUnion));
-        /* Zero the new private */
-        memset(&nprivs[idx], 0, sizeof(DevUnion));
-        pScr->privates = nprivs;
-    }
-    for (i = 0; i < xf86NumGPUScreens; i++) {
-        pScr = xf86GPUScreens[i];
-        nprivs = xnfreallocarray(pScr->privates,
-                                 xf86ScrnInfoPrivateCount, sizeof(DevUnion));
-        /* Zero the new private */
-        memset(&nprivs[idx], 0, sizeof(DevUnion));
-        pScr->privates = nprivs;
-    }
-    return idx;
 }
 
 Bool
