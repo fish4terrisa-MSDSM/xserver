369601048fa4b78ee1e0316cbc9816594f132f5d
Roman Gilg <subdiff@gmail.com>
2019-06-03 22:44:38 +0200

    present: Replace queued vblanks after new vblank is created
    This is preparation for auto-compositing, but also cleans up the code and
    improves the logic in the sense that other vblanks are only discarded
    when the new one was succesfully created. Otherwise we should still
    serve already queued ones.
    
    Signed-off-by: Roman Gilg <subdiff@gmail.com>

--- present/present_scmd.c
+++ present/present_scmd.c
@@ -628,6 +628,40 @@ present_execute(present_vblank_ptr vblank, uint64_t ust, uint64_t crtc_msc)
     present_execute_post(vblank, ust, crtc_msc);
 }
 
+/*
+ * Look for a matching presentation already on the list and
+ * don't bother doing the previous one if this one will overwrite it
+ * in the same frame.
+ */
+static void
+present_scmd_replace_queued(present_vblank_ptr vblank)
+{
+    present_window_priv_ptr window_priv = present_window_priv(vblank->window);
+    present_vblank_ptr      vbl, tmp;
+
+    if (vblank->update || !vblank->pixmap)
+        return;
+
+    xorg_list_for_each_entry_safe(vbl, tmp, &window_priv->vblank, window_list) {
+        if (!vbl->pixmap)
+            continue;
+
+        if (!vbl->queued)
+            continue;
+
+        if (vbl->crtc != vblank->crtc || vbl->target_msc != vblank->target_msc)
+            continue;
+
+        if (vbl == vblank)
+            continue;
+
+        present_vblank_scrap(vbl);
+
+        if (vbl->flip_ready)
+            present_re_execute(vbl);
+    }
+}
+
 static int
 present_scmd_pixmap(WindowPtr window,
                     PixmapPtr pixmap,
@@ -650,7 +684,7 @@ present_scmd_pixmap(WindowPtr window,
     uint64_t                    target_msc;
     uint64_t                    crtc_msc = 0;
     int                         ret;
-    present_vblank_ptr          vblank, tmp;
+    present_vblank_ptr          vblank;
     ScreenPtr                   screen = window->drawable.pScreen;
     present_window_priv_ptr     window_priv = present_get_window_priv(window, TRUE);
     present_screen_priv_ptr     screen_priv = present_screen_priv(screen);
@@ -686,30 +720,6 @@ present_scmd_pixmap(WindowPtr window,
                            divisor,
                            remainder);
 
-    /*
-     * Look for a matching presentation already on the list and
-     * don't bother doing the previous one if this one will overwrite it
-     * in the same frame
-     */
-
-    if (!update && pixmap) {
-        xorg_list_for_each_entry_safe(vblank, tmp, &window_priv->vblank, window_list) {
-
-            if (!vblank->pixmap)
-                continue;
-
-            if (!vblank->queued)
-                continue;
-
-            if (vblank->crtc != target_crtc || vblank->target_msc != target_msc)
-                continue;
-
-            present_vblank_scrap(vblank);
-            if (vblank->flip_ready)
-                present_re_execute(vblank);
-        }
-    }
-
     vblank = present_vblank_create(window,
                                    pixmap,
                                    serial,
@@ -730,6 +740,7 @@ present_scmd_pixmap(WindowPtr window,
     if (!vblank)
         return BadAlloc;
 
+    present_scmd_replace_queued(vblank);
     xorg_list_append(&vblank->event_queue, &present_exec_queue);
 
     if (msc_is_after(target_msc, crtc_msc)) {
