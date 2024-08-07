f72f70870fd1dcc1ca7e084208ca89ea83747a58
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-07 13:48:51 +0200

    Xext: saver: drop now obsolete swap procs
    Several SProc's have become no-ops, just calling the actual Proc's,
    so we can get rid of them entirely.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/saver.c
+++ Xext/saver.c
@@ -1268,12 +1268,6 @@ ProcScreenSaverDispatch(ClientPtr client)
 }
 
 static int _X_COLD
-SProcScreenSaverQueryVersion(ClientPtr client)
-{
-    return ProcScreenSaverQueryVersion(client);
-}
-
-static int _X_COLD
 SProcScreenSaverQueryInfo(ClientPtr client)
 {
     REQUEST(xScreenSaverQueryInfoReq);
@@ -1328,7 +1322,7 @@ SProcScreenSaverSuspend(ClientPtr client)
 }
 
 static int (*SwappedVector[]) (ClientPtr /* client */ ) = {
-SProcScreenSaverQueryVersion,
+        ProcScreenSaverQueryVersion,
         SProcScreenSaverQueryInfo,
         SProcScreenSaverSelectInput,
         SProcScreenSaverSetAttributes,
