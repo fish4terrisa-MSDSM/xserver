15d61ba50a4f0bb9094b50460e62dd8d0c20990a
Enrico Weigelt, metux IT consult <info@metux.net>
2024-09-10 15:28:00 +0200

    os: log: add syslog support
    Add support for logging to syslog.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/utils.c
+++ os/utils.c
@@ -77,6 +77,8 @@ __stdcall unsigned long GetTickCount(void);
 #define TRANS_REOPEN
 #include <X11/Xtrans/Xtrans.h>
 
+#include <libgen.h>
+
 #include "input.h"
 #include "dixfont.h"
 #include <X11/fonts/libxfont2.h>
@@ -496,6 +498,12 @@ ProcessCommandLine(int argc, char *argv[])
     }
     SeatId = getenv("XDG_SEAT");
 
+#ifdef CONFIG_SYSLOG
+    xorgSyslogIdent = getenv("SYSLOG_IDENT");
+    if (!xorgSyslogIdent)
+        xorgSyslogIdent = strdup(basename(argv[0]));
+#endif
+
     for (i = 1; i < argc; i++) {
         /* call ddx first, so it can peek/override if it wants */
         if ((skip = ddxProcessArgument(argc, argv, i))) {
@@ -865,6 +873,9 @@ ProcessCommandLine(int argc, char *argv[])
             else
                 UseMsg();
         }
+#ifdef CONFIG_SYSLOG
+        else if (ProcessCmdLineMultiInt(argc, argv, &i, "-syslogverbose", &xorgSyslogVerbosity));
+#endif
         else {
             ErrorF("Unrecognized option: %s\n", argv[i]);
             UseMsg();
